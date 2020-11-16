d44118a5147ae3d958b13c4398f0b73a96322f02
Aleksander D <aleksander.dorkov@gmail.com>
2020-11-16 15:36:08 +0200

    Update Resume and Theme Changer button

--- src/views/theme/ThemeBuilder.tsx
+++ src/views/theme/ThemeBuilder.tsx
@@ -29,9 +29,9 @@ function ThemeBuilder() {
                 <Paper className={'theme-paper'} elevation={15}>
                     <IconButton style={{padding: '5px'}} disabled={disabled}>
                         {darkTheme ?
-                            <Brightness7Icon style={{fontSize: '60px'}} onClick={() => dispatch(setLightTheme())}/>
+                            <Brightness7Icon style={{fontSize: '85px'}} onClick={() => dispatch(setLightTheme())}/>
                             :
-                            <Brightness4Icon style={{fontSize: '60px'}} onClick={() => dispatch(setDarkTheme())}/>
+                            <Brightness4Icon style={{fontSize: '85px'}} onClick={() => dispatch(setDarkTheme())}/>
                         }
                     </IconButton>
                 </Paper>
